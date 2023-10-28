29265857468881dab12ae0a3c93ceac746d1c75c
Stefan Iuga <stefan@iuga.email>
2023-10-28 11:20:26 +0300

    feat: add fizzbuzz

---
+++ session01/src/main/java/com/example/W01FizzBuzz.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class W01FizzBuzz {
+    private W01FizzBuzz() {
+    }
+
+    public static void showFizzBuzz(int n) {
+        for (int i = 0; i < n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        showFizzBuzz(100);
+    }
+}
