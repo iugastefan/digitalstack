cc28cdfdc68c465661f8ce90e2227b0976bd425f
Stefan Iuga <stefan@iuga.email>
2023-10-28 14:32:52 +0300

    feat: W06 add twoSum

--- session01/src/main/java/com/example/W06LinkedListMiddle.java
+++ session01/src/main/java/com/example/W06LinkedListMiddle.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import static com.example.W05LinkedList.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+import com.example.W05LinkedList.LinkedList;
 
 public class W06LinkedListMiddle {
     private W06LinkedListMiddle() {}
@@ -22,6 +25,17 @@ public class W06LinkedListMiddle {
         return byOne.data;
     }
 
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> cache = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (cache.containsKey(nums[i])) {
+                return new int[] {cache.get(nums[i]), i};
+            }
+            cache.put(target - nums[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
         assert middle(list.head) == null;
@@ -33,6 +47,26 @@ public class W06LinkedListMiddle {
         assert middle(list.head) == 3;
         list.add(6);
         assert middle(list.head) == 4;
+
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        assert result[0] == 0;
+        assert result[1] == 1;
+
+        nums = new int[] {3, 2, 4};
+        target = 6;
+        result = twoSum(nums, target);
+        assert result[0] == 1;
+        assert result[1] == 2;
+
+        nums = new int[] {3, 3};
+        target = 6;
+        result = twoSum(nums, target);
+        assert result[0] == 0;
+        assert result[1] == 1;
+
         System.out.println("All tests passed");
     }
 
